316f70d23121a678c4d1de709d4bc453a9c5bf10
Lisoferma <pilipeiko.sasha@gmail.com>
2023-09-07 23:28:14 +0900

    Add project files.

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,45 @@
+#include "TreeNode.h"
+#include <iostream>
+
+
+using namespace std;
+using namespace DSAABinaryTree;
+
+int main()
+{
+    TreeNode<int>* root = new TreeNode<int>(1);
+
+    root->Left = new TreeNode<int>(2);
+    root->Right = new TreeNode<int>(3);
+
+    root->Left->Left = new TreeNode<int>(4);
+    root->Left->Right = new TreeNode<int>(5);
+
+    // Полученное дерево:
+    //           1
+    //          / \
+    //         2   3
+    //        / \
+    //       4   5
+
+    cout << root->Left->Left->Data; // 4
+
+    //DeleteBinaryTree(root);
+
+    //cout << root->Data;
+
+    return 0;
+}
+
+
+/// <summary>
+/// Удалить бинарное дерево, root будет равен nullptr.
+/// </summary>
+/// <typeparam name="T">Тип данных которое хранит дерево.</typeparam>
+/// <param name="root">Корень дерева.</param>
+template <typename T>
+void DeleteBinaryTree(TreeNode<typename T>* root)
+{
+    if (root == nullptr) return;
+    delete root;
+}
---
+++ BinaryTree/TreeNode.h
@@ -0,0 +1,53 @@
+#pragma once
+
+namespace DSAABinaryTree
+{
+	/// <summary>
+	///	Узел бинарного дерева.
+	/// </summary>
+	/// <typeparam name="T">
+	///	Тип данных который хранит узел.
+	/// </typeparam>
+	template<typename T>
+	class TreeNode
+	{
+	public:
+		// Данные узла
+		T Data;
+
+		// Указатель на родительский узел
+		TreeNode* Parent;
+
+		// Указатель на дочерний левый узел
+		TreeNode* Left;
+
+		// Указатель на дочерний правый узел
+		TreeNode* Right;
+
+		TreeNode() = default;
+
+		/// <summary>
+		///	Инициализировать данные узла.
+		/// </summary>
+		/// <param name="data">Данные узла.</param>
+		TreeNode(T data);
+
+
+		/// <summary>
+		///	Инициализировать все поля узла.
+		/// </summary>
+		/// <param name="data">Данные узла.</param>
+		/// <param name="parent">Указатель на родительский узел.</param>
+		/// <param name="left">Указатель на левый узел.</param>
+		/// <param name="right">Указатель на правый узел.</param>
+		TreeNode(T data, TreeNode* parent, TreeNode* left, TreeNode* right);
+
+		~TreeNode();		
+
+		TreeNode(const T& Data, TreeNode* Parent, TreeNode* Left, TreeNode* Right)
+			: Data(Data), Parent(Parent), Left(Left), Right(Right)
+		{
+		}
+	};
+}  // namespace DSAABinaryTree
+
