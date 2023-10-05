cee8e17c5dbb86b9a9c13e73d2a12c87f0c50fec
Lisoferma <pilipeiko.sasha@gmail.com>
2023-10-05 18:58:47 +0900

    Delete unused BinarySearchTree.cpp

--- BinaryTree/BinarySearchTree.h
+++ BinaryTree/BinarySearchTree.h
@@ -6,11 +6,68 @@
 
 namespace DSAABinaryTree
 {
+	template<typename T>
 	class BinarySearchTree
 	{
-	public:
-
 	private:
+		// Корень двоичного дерева поиска
+		BinaryNode<T>* root;
 
+	public:
+        BinarySearchTree() : root(nullptr)
+        { }
+
+
+        /// <summary>
+        /// Инициализировать корень дерева.
+        /// </summary>
+        /// <param name="root">Корень дерева.</param>
+        BinarySearchTree(BinaryNode<T>* root) : root(root)
+        { }
+
+
+        /// <summary>
+        /// Инициализировать дерево вектором.
+        /// </summary>
+        /// <param name="dataVector">Вектор который хранит данные для узлов.</param>
+        BinarySearchTree(const std::vector<T>& dataVector)
+        {
+            for (T data : dataVector)
+            {
+                Insert(data);
+            }
+        }
+
+
+        /// <summary>
+        /// Вставить данные в дерево.
+        /// </summary>
+        /// <param name="data">Данные для нового узла.</param>
+        void Insert(const T& data)
+        {
+            BinaryNode<T>* current = root;
+
+            while (current && current->Data != data)
+            {
+                if (current->Data > data && current->Left == NULL)
+                {
+                    current->Left = new BinaryNode<T>(data);
+                    return;
+                }
+                if (current->Data < data && current->Right == NULL)
+                {
+                    current->Right = new BinaryNode<T>(data);
+                    return;
+                }
+                if (current->Data > data)
+                {
+                    current = current->Left;
+                }
+                else
+                {
+                    current = current->Right;
+                }                 
+            }
+        }
 	};
 }
