afa8e3138052ed9d9bb4f6781476ad3114585e81
Lisoferma <pilipeiko.sasha@gmail.com>
2023-10-13 18:57:35 +0900

    Add constructor tests

--- BinaryTree/Tests/BinaryTreeTests/BinarySearchTreeTests.cpp
+++ BinaryTree/Tests/BinaryTreeTests/BinarySearchTreeTests.cpp
@@ -1,7 +1,35 @@
 #include "pch.h"
+#include "../../BinarySearchTree.h"
+#include "../../BinaryTreeService.h"
+#include <vector>
 
-TEST(TestCaseName, TestName)
+using namespace DSAABinaryTree;
+
+TEST(Constructor, NoArguments_InitializeNullptr)
 {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	BinarySearchTree<int> bst;
+
+	EXPECT_EQ(bst.GetRoot(), nullptr);
 }
+
+
+TEST(Constructor, ArgumentRoot_InitializeRoot)
+{
+	BinaryNode<int>* initializeRoot = new BinaryNode<int>(5);
+	BinarySearchTree<int> bst(initializeRoot);
+
+	EXPECT_EQ(bst.GetRoot(), initializeRoot);
+}
+
+
+//TEST(Constructor, ArgumentVector_InitializeTreeFromVector)
+//{
+//	std::vector<int> vectorInitialize = { 1, 2, 3, 4, 5, 6 };
+//	BinarySearchTree<int> bst(vectorInitialize);
+//
+//	std::vector<int> vectorFromTree;
+//	BinaryTreeService::InorderTraversal<int>(bst.GetRoot(), [&vectorFromTree](BinaryNode<int>& node) {
+//		vectorFromTree.push_back(node.Data); });
+//
+//	EXPECT_EQ(vectorInitialize, vectorFromTree);
+//}
