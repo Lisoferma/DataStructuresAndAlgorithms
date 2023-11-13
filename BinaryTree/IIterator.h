a7a37922f56b670de572171bd78bdfb9d6482bc7
Lisoferma <pilipeiko.sasha@gmail.com>
2023-11-13 20:23:17 +0900

    Add comments

--- BinaryTree/IIterator.h
+++ BinaryTree/IIterator.h
@@ -1,7 +1,13 @@
+//@author Lisoferma
+
 #pragma once
 
 namespace DSAABinaryTree
 {
+    /// <summary>
+    /// Предоставляет методы для взаимодействия с итератором.
+    /// </summary>
+    /// <typeparam name="T">Тип данных итерируемой коллекции.</typeparam>
     template <typename T>
     class IIterator
     {
@@ -10,12 +16,26 @@ namespace DSAABinaryTree
 
         virtual bool operator !=(const IIterator<T>& iter) const  = 0;
 
+        /// <summary>
+        /// Получить данные из коллекции по текущему указателю итератора.
+        /// </summary>
         virtual T& operator *() const = 0;
 
+        /// <summary>
+        /// Перейти к следующему элементу коллекции.
+        /// </summary>
+        /// <returns>Итератор после перехода к следующему элементу.</returns>
         virtual IIterator<T>& operator ++() = 0;
 
+        /// <summary>
+        /// Перейти к следующему элементу коллекции.
+        /// </summary>
+        /// <returns>Итератор перед переходом к следующему элементу.</returns>
         virtual IIterator<T>& operator ++(int) = 0;
 
+        /// <summary>
+        /// Итератор дошёл до конца коллекции.
+        /// </summary>
         virtual bool IsEnd() const = 0;
     };
 }
