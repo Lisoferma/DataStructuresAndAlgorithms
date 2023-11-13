aaa6a0e6ec773ccf958b6a84592a2943ab03b86f
Lisoferma <pilipeiko.sasha@gmail.com>
2023-11-13 20:23:36 +0900

    Add comments

--- BinaryTree/InorderIterator.h
+++ BinaryTree/InorderIterator.h
@@ -1,4 +1,6 @@
-﻿#pragma once
+﻿//@author Lisoferma
+
+#pragma once
 
 #include "IIterator.h"
 #include "BinaryNode.h"
@@ -6,10 +8,15 @@
 
 namespace DSAABinaryTree
 {
+    /// <summary>
+    /// Итератор для упорядоченого (в порядке LNR) прохода по дереву.
+    /// </summary>
+    /// <typeparam name="T">Тип данных итерируемой коллекции.</typeparam>
     template<typename T>
     class InorderIterator : public IIterator<T>
     {
     public:
+        // Свойства итератора для STL
         using iterator_category = std::forward_iterator_tag;
         using value_type        = T;
         using difference_type   = T;
@@ -17,6 +24,11 @@ namespace DSAABinaryTree
         using reference         = T&;
 
 
+        /// <summary>
+        /// Инициализировать итератор для дерева.
+        /// </summary>
+        /// <param name="node">Узел на который указывает итератор.</param>
+        /// <param name="setToStart">Установить на начальное (begin) положение итератора.</param>
         InorderIterator(BinaryNode<T>* node, bool setToStart = true) : _current(node)
         {
             if (setToStart)
@@ -44,12 +56,19 @@ namespace DSAABinaryTree
         }
 
 
+        /// <summary>
+        /// Получить данные из коллекции по текущему указателю итератора.
+        /// </summary>
         T& operator *() const override
         {
             return _current->Data;
         }
 
 
+        /// <summary>
+        /// Перейти к следующему элементу коллекции.
+        /// </summary>
+        /// <returns>Итератор после перехода к следующему элементу.</returns>
         InorderIterator<T>& operator++() override
         {
             Next();
@@ -57,6 +76,10 @@ namespace DSAABinaryTree
         }
 
 
+        /// <summary>
+        /// Перейти к следующему элементу коллекции.
+        /// </summary>
+        /// <returns>Итератор перед переходом к следующему элементу.</returns>
         InorderIterator<T>& operator++(int) override
         {
             InorderIterator<T> temp(_current, false);
@@ -65,6 +88,9 @@ namespace DSAABinaryTree
         }
 
 
+        /// <summary>
+        /// Итератор дошёл до конца коллекции.
+        /// </summary>
         bool IsEnd() const override
         {
             return _current == nullptr;
@@ -79,12 +105,19 @@ namespace DSAABinaryTree
         std::stack< BinaryNode<T>* > _traversal;
 
 
+        /// <summary>
+        /// Проверить оба ли итератора дошли до конца коллекции.
+        /// </summary>
         bool BothIsEnd(const IIterator<T>& iter1, const IIterator<T>& iter2) const
         {
             return iter1.IsEnd() && iter2.IsEnd();
         }
 
 
+        /// <summary>
+        /// Установить указател итератора на крайний левый узел от указанного.
+        /// </summary>
+        /// <param name="node">Узел от которого идти влево.</param>
         void MoveLeft(BinaryNode<T>* node)
         {
             while (node)
@@ -97,12 +130,18 @@ namespace DSAABinaryTree
         }
 
 
+        /// <summary>
+        /// Есть следующие элементы в коллекции.
+        /// </summary>
         bool HasNext() const
         {
             return !_traversal.empty();
         }
 
 
+        /// <summary>
+        /// Установить указатель итератора на следующий элемент в дереве.
+        /// </summary>
         void Next()
         {
             if (!HasNext())
