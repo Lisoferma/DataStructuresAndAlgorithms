9bb5a381302be6ca5c780c645dbe56311ae4d71c
Lisoferma <pilipeiko.sasha@gmail.com>
2023-11-13 20:23:04 +0900

    Add comments

--- BinaryTree/IIterated.h
+++ BinaryTree/IIterated.h
@@ -1,14 +1,26 @@
+//@author Lisoferma
+
 #pragma once
 
 #include "IIterator.h"
 
 namespace DSAABinaryTree
 {
+	/// <summary>
+	/// Предоставляет методы для доступа к итератору.
+	/// </summary>
+	/// <typeparam name="T">Тип данных итерируемой коллекции.</typeparam>
 	template<typename T>
 	class IIterated
 	{
+		/// <summary>
+		/// Получить итератор в начале коллекции.
+		/// </summary>
 		virtual IIterator<T>& begin() = 0;
 
+		/// <summary>
+		/// Получить итератор в конце коллекции.
+		/// </summary>
 		virtual IIterator<T>& end() = 0;
 	};
 }
