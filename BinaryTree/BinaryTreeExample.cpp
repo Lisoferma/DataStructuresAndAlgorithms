d34e0982fe10f436a888879d73ce2ac14b3c52c1
Lisoferma <pilipeiko.sasha@gmail.com>
2023-11-16 22:30:53 +0900

    Add std algorithm example

--- BinaryTree/BinaryTreeExample.cpp
+++ BinaryTree/BinaryTreeExample.cpp
@@ -6,6 +6,7 @@
 #include "BinaryTreeService.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 using namespace DSAABinaryTree;
@@ -130,7 +131,9 @@ int main()
     //// Удаление всего дерева
     //bst.Delete();
     //cout << "\n\nBST is empty after delete: " << bst.IsEmpty() << endl;
-        
+    
+
+    // Применение итератора
     cout << "\n\nIterator: ";
 
     for (int item : bst)
@@ -138,18 +141,46 @@ int main()
         cout << item;
     }
 
-    // Другие способы итерирования:
 
-    //auto iterator = bst.begin();
-    //auto end = bst.end();
+    // Использование std algorithm
+    cout << "\n\nAlgorithm: ";
+   
 
-    //while (iterator != end)
-    //{
-    //    cout << *iterator << " " << endl;
-    //    ++iterator;
-    //}
+    cout << "\n for_each: ";
+    std::for_each(bst.begin(), bst.end(), [](auto item) { std::cout << item << " "; });
 
-    // std::for_each(bst.begin(), bst.end(), [](auto item) { std::cout << item << " "; });
+
+    cout << "\n any_of: ";
+    if ( std::any_of(bst.begin(), bst.end(), [](int i) { return i % 2 == 0; }) )
+        std::cout << "There are even numbers";
+    else
+        std::cout << "There are no even numbers";
+
+
+    cout << "\n all_of: ";
+    if (std::all_of(bst.begin(), bst.end(), [](int i) { return i % 2 == 0; }))
+        std::cout << "All numbers are even";
+    else
+        std::cout << "Not all numbers are even";
+
+
+    cout << "\n none_of: ";
+    if (std::none_of(bst.begin(), bst.end(), [](int i) { return i == 8; }))
+        std::cout << "No number is equal to 8";
+    else
+        std::cout << "There is a number equal to 8";
+
+
+    cout << "\n transform: ";
+    std::transform(bst.begin(), bst.end(), bst.begin(), [](int i) { return i * 2; });
+    for (int item : bst) cout << item << " ";
+
+
+    cout << "\n copy_if: ";
+    std::vector<int> vector(2);
+    std::copy_if(bst.begin(), bst.end(), vector.begin(), [](int i) { return i % 4 == 0; });
+    for (int item : vector) cout << item << " ";
+
 
     return 0;
 }
