a7ec48c92d5ee7e3898ee9c3b67e582560e5a315
Lisoferma <pilipeiko.sasha@gmail.com>
2023-12-02 19:20:17 +0900

    Add example

--- Heap/Heap/HeapExample.cpp
+++ Heap/Heap/HeapExample.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "Heap.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Создать кучу целых чисел
+    Heap<int> integerHeap;
+
+    // Вставка элементов
+    integerHeap.Insert(10);
+    integerHeap.Insert(5);
+    integerHeap.Insert(7);
+    integerHeap.Insert(15);
+    integerHeap.Insert(4);
+    integerHeap.Insert(200);
+    integerHeap.Insert(1);
+
+    // Удаление элементов
+    integerHeap.Remove(7);
+    integerHeap.Remove(1);
+
+    // Получить минимальный элемент
+    std::cout << "Top element: " << integerHeap.Top() << std::endl;
+
+    // Удалить минимальный элемент
+    integerHeap.Pop();
+
+    // Получить минимальный элемент после извлечения предыдущего
+    std::cout << "Top element after Pop(): " << integerHeap.Top() << std::endl;
+
+    // Проверка на пустую кучу
+    if (integerHeap.IsEmpty())
+    {
+        std::cout << "Heap is empty" << std::endl;
+    }
+    else
+    {
+        std::cout << "Heap is not empty" << std::endl;
+    }
+
+    return 0;
 }
