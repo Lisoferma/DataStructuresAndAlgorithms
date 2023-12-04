4f553bd570fdddb33b660b598e05be3a6b9043da
Lisoferma <pilipeiko.sasha@gmail.com>
2023-12-05 00:53:24 +0900

    Add sort test

---
+++ Sort/SortTest/SortTests.cpp
@@ -0,0 +1,16 @@
+// @author Lisoferma
+
+#include <vector>
+#include "pch.h"
+#include "../Sort/Sort.h"
+
+
+TEST(HeapSort, SortFilledVector_VectorBecameHeap)
+{
+	std::vector<int> vector{ 12, 11, 13, 5, 6, 7, 20, -5, -6, 0 };
+
+	HeapSort(vector);
+
+	for (int i = 0; i < vector.size() - 1; ++i)
+		EXPECT_TRUE(vector[i] <= vector[i + 1]);
+}
